f57ca94a27bcb00fffe2a0a952f1fe3b64c45c53
Karthik Senthil <karthiksenthil@Prushas-MacBook-Air.local>
2022-01-19 16:19:06 -0500

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,36 @@
 <template>
+<wallet-provider :wallets="wallets">
+    <wallet-modal-provider>
+      <wallet-multi-button/>
+    </wallet-modal-provider>  
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+</wallet-provider>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import HelloWorld from './components/HelloWorld.vue';
 
+import { getPhantomWallet, getSolflareWallet } from '@solana/wallet-adapter-wallets';
+import { WalletMultiButton, WalletModalProvider } from '@solana/wallet-adapter-vue-ui';
+
+
+// eslint-disable-next-line no-unused-vars
+import { WalletProvider } from '@solana/wallet-adapter-vue';
+
+// eslint-disable-next-line no-unused-vars
+//const wallets =[];
+const wallets = [
+    getPhantomWallet(),
+    getSolflareWallet(),
+];  
+
+
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld, WalletProvider, WalletMultiButton, WalletModalProvider
   }
 });
 </script>
