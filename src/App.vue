fca7c7ea568e7c7763bdcd219aacfeb688f8a13e
Karthik Senthil <karthiksenthil@Prushas-MacBook-Air.local>
2022-02-09 17:47:44 -0500

    updating

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,55 @@
 <template>
-<wallet-provider :wallets="wallets">
-    <wallet-modal-provider>
-      <wallet-multi-button/>
-    </wallet-modal-provider>  
+
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-</wallet-provider>
+
+    <wallet-multi-button></wallet-multi-button>
+
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+//import { createApp } from 'vue';
+//import { initWallet } from 'solana-wallets-vue';
+import { WalletMultiButton } from 'solana-wallets-vue'
+
+
 import HelloWorld from './components/HelloWorld.vue';
+//import 'solana-wallets-vue/styles.css';
 
-import { getPhantomWallet, getSolflareWallet } from '@solana/wallet-adapter-wallets';
-import { WalletMultiButton, WalletModalProvider } from '@solana/wallet-adapter-vue-ui';
+//import { getPhantomWallet, getSolflareWallet } from '@solana/wallet-adapter-wallets';
+//import { WalletMultiButton, WalletModalProvider } from '@solana/wallet-adapter-vue-ui';
 
+/*import {
+  PhantomWalletAdapter,
+  SlopeWalletAdapter,
+  SolflareWalletAdapter,
+} from '@solana/wallet-adapter-wallets';
+*/
 
 // eslint-disable-next-line no-unused-vars
-import { WalletProvider } from '@solana/wallet-adapter-vue';
+//import { WalletProvider } from '@solana/wallet-adapter-vue';
 
 // eslint-disable-next-line no-unused-vars
 //const wallets =[];
-const wallets = [
+/*const wallets = [
     getPhantomWallet(),
     getSolflareWallet(),
 ];  
 
 
+*/
+
+
+
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld, WalletProvider, WalletMultiButton, WalletModalProvider
+    HelloWorld, WalletMultiButton
+  }, setup() {
+   // initWallet(walletOptions);
   }
-});
+}); 
 </script>
 
 <style>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,31 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { initWallet } from 'solana-wallets-vue';
 
-createApp(App).mount('#app')
+import SolanaWallets from 'solana-wallets-vue';
+
+// You can either import the default styles or create your own.
+import 'solana-wallets-vue/styles.css';
+
+import {
+  PhantomWalletAdapter,
+  SlopeWalletAdapter,
+  SolflareWalletAdapter,
+} from '@solana/wallet-adapter-wallets';
+
+const walletOptions = {
+    wallets: [
+      new PhantomWalletAdapter(),
+      new SlopeWalletAdapter(),
+  //    new SolflareWalletAdapter({ network: 'devnet' }),
+    ],
+    autoConnect: true,
+  }
+
+initWallet(walletOptions);
+
+createApp(App)
+//.use(SolanaWallets, walletOptions)
+.mount('#app');
+
+
